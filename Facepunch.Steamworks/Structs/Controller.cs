ea1d7f59e5852f13e4b0dcc13a6adac462018908
Herman Groenenboom <taartaar@gmail.com>
2023-04-07 10:54:45 +0200

    Add bindings for retrieving InputActionOrigins for a Controller.

--- Facepunch.Steamworks/Structs/Controller.cs
+++ Facepunch.Steamworks/Structs/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using Steamworks.Data;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Steamworks
@@ -15,6 +16,7 @@ namespace Steamworks
 
 		public ulong Id => Handle.Value;
 		public InputType InputType => SteamInput.Internal.GetInputTypeForHandle( Handle );
+		internal InputActionSetHandle_t ActionSetHandle => SteamInput.Internal.GetCurrentActionSet( Handle );
 
 		/// <summary>
 		/// Reconfigure the controller to use the specified action set (ie 'Menu', 'Walk' or 'Drive')
@@ -24,12 +26,43 @@ namespace Steamworks
 		public string ActionSet
 		{
 			set => SteamInput.Internal.ActivateActionSet( Handle, SteamInput.GetActionSetHandle( value ) );
-		}
+		}		
 
 		public void DeactivateLayer( string layer ) => SteamInput.Internal.DeactivateActionSetLayer( Handle, SteamInput.GetActionSetHandle( layer ) );
 		public void ActivateLayer( string layer ) => SteamInput.Internal.ActivateActionSetLayer( Handle, SteamInput.GetActionSetHandle( layer ) );
 		public void ClearLayers() => SteamInput.Internal.DeactivateAllActionSetLayers( Handle );
 
+		#region Action Origins	
+
+		/// <summary>
+		/// Get the origin(s) for an analog action within an action set.
+		/// Use this to display the appropriate on-screen prompt for the action.
+		/// <para>
+		/// This is cheap, and Valve recommends you re-gather the origins each frame to update your ingame prompts in case they have changed.
+		/// </para>
+		/// </summary>
+		public IEnumerable<InputActionOrigin> GetAnalogActionOrigins( string actionName )
+		{
+			InputActionOrigin[] actionOrigins = new InputActionOrigin[SteamInput.STEAM_INPUT_MAX_ORIGINS];
+			int numOrigins = SteamInput.Internal.GetAnalogActionOrigins( Handle, ActionSetHandle, SteamInput.Internal.GetAnalogActionHandle( actionName ), actionOrigins );
+			return actionOrigins.Take( numOrigins );
+		}
+
+		/// <summary>
+		/// Get the origin(s) for a digital action within an action set.
+		/// Use this to display the appropriate on-screen prompt for the action.
+		/// <para>
+		/// This is cheap, and Valve recommends you re-gather the origins each frame to update your ingame prompts in case they have changed.
+		/// </para>
+		/// </summary>
+		public IEnumerable<InputActionOrigin> GetDigitalActionOrigins( string actionName )
+		{
+			InputActionOrigin[] actionOrigins = new InputActionOrigin[SteamInput.STEAM_INPUT_MAX_ORIGINS];
+			int numOrigins = SteamInput.Internal.GetDigitalActionOrigins( Handle, ActionSetHandle, SteamInput.Internal.GetDigitalActionHandle( actionName ), actionOrigins );
+			return actionOrigins.Take( numOrigins );
+		}
+
+		#endregion Action Origins
 
 		/// <summary>
 		/// Returns the current state of the supplied digital game action
